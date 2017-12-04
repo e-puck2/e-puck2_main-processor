4d4ba46e4d861aabc7c40c393fca289ad59ff2f2
dburnier <danielburnier@gmail.com>
2017-12-04 15:29:45 +0100

    Manage board files from board config file : board.chcfg
    PLEASE : Don't edit manually these following files : board.c, board.h and board.mk
    These files have been automatically generated using ChibiStudio board generator plugin

--- ChibiOS_ext/os/hal/boards/epuck2/board.mk
+++ ChibiOS_ext/os/hal/boards/epuck2/board.mk
@@ -1,5 +1,5 @@
 # List of all the board related files.
-BOARDSRC = ${CHIBIOS_EXT}/os/hal/boards/epuck2/board.c
+BOARDSRC = $(CHIBIOS_EXT)/os/hal/boards/epuck2/board.c
 
 # Required include directories
-BOARDINC = ${CHIBIOS_EXT}/os/hal/boards/epuck2
+BOARDINC = $(CHIBIOS_EXT)/os/hal/boards/epuck2

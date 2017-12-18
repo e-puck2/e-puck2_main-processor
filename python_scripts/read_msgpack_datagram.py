9a0ce292eace6dc8fd47db3ad532ca9877ae5052
Eliot Ferragni <eliot.ferragni@epfl.ch>
2017-12-18 16:00:56 +0100

    -Added a python3 script example to read the messages sent by the e-puck2

---
+++ python_scripts/read_msgpack_datagram.py
@@ -0,0 +1,54 @@
+import serial
+import serial_datagram
+import sys
+import os
+import msgpack
+
+#this script needs Python 3, pyserial and msgpack to run
+#pip3 install pyserial
+#pip3 install msgpack-python
+
+#the script need the serial port to open as arg when executing it
+def main():
+    if len(sys.argv) > 2:
+        baud = sys.argv[2]
+    else:
+        baud = 115200
+
+    if len(sys.argv) == 1:
+        print('Please give the serial port to use')
+        return
+    else:
+        fdesc = serial.Serial(sys.argv[1], baudrate=baud)
+
+    while True:
+        try:
+            #return a complete decoded frame
+            dtgrm = serial_datagram.read(fdesc)
+            #unpack the message pack frame
+            data = msgpack.unpackb(dtgrm, use_list=False, encoding='utf-8') 
+
+            #print acc datas only if they are present in the frame received
+            field_to_found = 'acc'
+            if(field_to_found in data):
+                buf = data[field_to_found]
+                print('accelerometer : x = ',buf[0],', y = ',buf[1],', z = ',buf[2])
+
+            #print gyro datas only if they are present in the frame received
+            field_to_found = 'gyro'
+            if(field_to_found in data):
+                buf = data[field_to_found]
+                print('gyro : x = ',buf[0],', y = ',buf[1],', z = ',buf[2])
+
+            #print time data only if they are present in the frame received
+            field_to_found = 'time'
+            if(field_to_found in data):
+                time = data[field_to_found]
+                print('time = ',time)
+
+        except (serial_datagram.CRCMismatchError, serial_datagram.FrameError):
+            sys.stderr.write("CRC error")
+
+
+if __name__ == '__main__':
+    main()

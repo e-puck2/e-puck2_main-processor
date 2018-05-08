363ab6054b4ea9889088d453159b780b0b1e24ab
Eliot Ferragni <eliot.ferragni@epfl.ch>
2018-05-08 10:51:59 +0200

    -[ADC] - Enhanced the handling of the battery measurement

--- src/sensors/battery_level.c
+++ src/sensors/battery_level.c
@@ -19,37 +19,21 @@
 
 #define COEFF_ADC_TO_VOLT       ((1.0f * ADC_RESOLUTION * VOLTAGE_DIVIDER) / VREF) //convertion from adc value to voltage
 
-static uint16_t adc_value;
+#define COEFF_A                 0.8f
+#define COEFF_B                 0.2f
+
+static uint32_t adc_value;
 static adcsample_t adc_samples[DMA_BUFFER_SIZE];
 static BSEMAPHORE_DECL(measurement_ready, true);
 static battery_msg_t battery_value;
 
 /***************************INTERNAL FUNCTIONS************************************/
 
- /**
- * @brief   Callback called after an ADC measure which 
- *          averages and copies the measure
- */
-static void adc_cb(ADCDriver *adcp, adcsample_t *samples, size_t n)
-{
-	(void) adcp;
-
-    /* Calculate the average over all samples. */
-	adc_value = 0;
-    for (size_t i = 0; i < n; i++) {
-    	adc_value += samples[i];
-    }
-    adc_value /= n;
-
-    chSysLockFromISR();
-    adcStopConversionI(adcp);
-    chBSemSignalI(&measurement_ready);
-    chSysUnlockFromISR();
-}
+static void adc_cb(ADCDriver *adcp, adcsample_t *samples, size_t n);
 
 //ADC configuration
 static const ADCConversionGroup group = {
-    .circular = true,
+    .circular = false,
     .num_channels = 1,
     .end_cb = adc_cb,
     .error_cb = NULL,
@@ -62,6 +46,27 @@ static const ADCConversionGroup group = {
     .sqr2 = 0,
     .sqr3 = ADC_SQR3_SQ1_N(1),
 };
+
+ /**
+ * @brief   Callback called after an ADC measure which 
+ *          averages and copies the measure
+ */
+static void adc_cb(ADCDriver *adcp, adcsample_t *samples, size_t n)
+{
+    (void)adcp;
+
+    /* Calculate the average over all samples. */
+    adc_value = 0;
+    for (size_t i = 0; i < n; i++) {
+        adc_value += samples[i];
+    }
+    adc_value /= n;
+
+    chSysLockFromISR();
+    adcStartConversionI(&ADCD1, &group, adc_samples, DMA_BUFFER_SIZE);
+    chBSemSignalI(&measurement_ready);
+    chSysUnlockFromISR();
+}
 
  /**
  * @brief   Thread which updates the measures and publishes them
@@ -78,21 +83,21 @@ static THD_FUNCTION(battery_thd, arg)
     messagebus_advertise_topic(&bus, &battery_topic, "/battery_level");
     uint8_t first_value = 1;
 
-    while (true) {
-        adcAcquireBus(&ADCD1);
+    //we never release it
+    adcAcquireBus(&ADCD1);
+    adcStartConversion(&ADCD1, &group, adc_samples, DMA_BUFFER_SIZE);
 
-        /* Starts a measurement and waits for it to complete. */
-        adcStartConversion(&ADCD1, &group, adc_samples, DMA_BUFFER_SIZE);
+    /* Starts a measurement and waits for it to complete. */
+    while (true) {
         chBSemWait(&measurement_ready);
-        adcReleaseBus(&ADCD1);
 
         /* Converts the measurement to volts and publish the measurement on the
          * bus. */
         if(first_value) {
-        	first_value = 0;
-        	battery_value.raw_value = adc_value;
+            first_value = 0;
+            battery_value.raw_value = adc_value;
         } else {
-        	battery_value.raw_value = (float)(battery_value.raw_value)*0.8 + (float)(adc_value)*0.2;
+            battery_value.raw_value = (float)(battery_value.raw_value)*COEFF_A+ (float)(adc_value)*COEFF_B;
         }
         battery_value.voltage = battery_value.raw_value / COEFF_ADC_TO_VOLT;
         battery_value.percentage =  (battery_value.voltage - MIN_VOLTAGE) * 
@@ -100,7 +105,7 @@ static THD_FUNCTION(battery_thd, arg)
                                     (MAX_VOLTAGE - MIN_VOLTAGE) + MIN_PERCENTAGE;
         messagebus_topic_publish(&battery_topic, &battery_value, sizeof(battery_value));
 
-    	//battery_check();
+        //battery_check();
 
         /* Sleep for some time. */
         chThdSleepMilliseconds(500);
@@ -114,7 +119,7 @@ static THD_FUNCTION(battery_thd, arg)
 
 void battery_level_start(void)
 {
-	adcStart(&ADCD1, NULL);
+    adcStart(&ADCD1, NULL);
     static THD_WORKING_AREA(battery_thd_wa, 1024);
     chThdCreateStatic(battery_thd_wa, sizeof(battery_thd_wa), NORMALPRIO, battery_thd, NULL);
 }
@@ -132,17 +137,17 @@ float get_battery_percentage(void){
 }
 
 void battery_check(void) {
-	if(battery_value.raw_value < BATTERY_LEVEL_LOW) {
-		set_led(LED1, 1);
-	} else {
-		set_led(LED1, 0);
-	}
+    if(battery_value.raw_value < BATTERY_LEVEL_LOW) {
+        set_led(LED1, 1);
+    } else {
+        set_led(LED1, 0);
+    }
 
-	if(battery_value.raw_value < BATTERY_LEVEL_OFF) {
-		set_led(LED3, 1);
-	} else {
-		set_led(LED3, 0);
-	}
+    if(battery_value.raw_value < BATTERY_LEVEL_OFF) {
+        set_led(LED3, 1);
+    } else {
+        set_led(LED3, 0);
+    }
 }
 
 /**************************END PUBLIC FUNCTIONS***********************************/

628ae75948bea8ab664e7991d831a7d9e323480d
GCtronic <stefano@gctronic.com>
2018-12-04 09:32:55 +0100

    Small improvements
    * epuck1x library: suspend SPI communication when grabbing an image
    * SPI communication:
    - support suspend/resume
    - support enabling/disabling image transfer
    - support enabling/disabling RGB setting
    * main:
    - selector 8: disable image transfer in SPI to avoid conflicts
    - selector 15: changed camera settings to have a wider view in the image
    (use zoom=4)

--- src/spi_comm.c
+++ src/spi_comm.c
@@ -11,6 +11,10 @@
 uint8_t spi_rx_buff[SPI_PACKET_MAX_SIZE];
 uint8_t spi_tx_buff[SPI_PACKET_MAX_SIZE]; //12]; //SPI_PACKET_MAX_SIZE];
 uint8_t *last_img_ptr = NULL;
+uint8_t image_transfer_enabled = 0;
+uint8_t rgb_setting_enabled = 0;
+uint8_t spi_suspend_flag = 0;
+static thread_t *spi_thd_ptr;
 
 /*
  * SPI communication thread.
@@ -86,19 +90,25 @@ static THD_FUNCTION(spi_thread, p) {
 
 	while (true) {
 
-		// Avoid using DCMI and SPI at the same time because there is an hardware bug with DMA.
-		dcmi_capture_start();
-		wait_image_ready();
+		chSysLock();
+		if(spi_suspend_flag == 1) {
+			chSchGoSleepS(CH_STATE_SUSPENDED);
+		}
+	    chSysUnlock();
+
+		if(image_transfer_enabled == 1) {
+			// Avoid using DCMI and SPI at the same time because there is an hardware bug with DMA.
+			// Therefore we need to first grab an image and then send it through SPI while the DCMI is stopped.
+			dcmi_capture_start();
+			wait_image_ready();
+		}
 
 		memset(spi_rx_buff, 0xFF, SPI_PACKET_MAX_SIZE);
-		get_all_rgb_state(&spi_tx_buff[0]);
-//		spi_tx_buff[0] = 50;
-//		spi_tx_buff[4] = 50;
-//		spi_tx_buff[8] = 50;
-//		spi_tx_buff[9] = 0;
-//		spi_tx_buff[10] = 50;
-//		spi_tx_buff[11] = 50;
-
+		if(rgb_setting_enabled == 1) {
+			get_all_rgb_state(&spi_tx_buff[0]);
+		} else {
+			memset(spi_tx_buff, 101, 12); // When the intensity value of the RGB LEDs is out of range, then their state isn't changed.
+		}
 
 		spiSelect(&SPID1);
 		spiExchange(&SPID1, 12, spi_tx_buff, spi_rx_buff);
@@ -111,48 +121,50 @@ static THD_FUNCTION(spi_thread, p) {
 
 		button_set_state(spi_rx_buff[0]);
 
-		if(spi_rx_buff[1] == 0xB7) { // -'I' => camera image
+		if(image_transfer_enabled == 1) {
+			if(spi_rx_buff[1] == 0xB7) { // -'I' => camera image
 
-			numPackets = po8030_get_image_size()/SPI_PACKET_MAX_SIZE;
-			remainingBytes = po8030_get_image_size()%SPI_PACKET_MAX_SIZE;
-			rx_err = 0;
+				numPackets = po8030_get_image_size()/SPI_PACKET_MAX_SIZE;
+				remainingBytes = po8030_get_image_size()%SPI_PACKET_MAX_SIZE;
+				rx_err = 0;
 
-			last_img_ptr = dcmi_get_last_image_ptr();
+				last_img_ptr = dcmi_get_last_image_ptr();
 
-//			memset(spi_tx_buff, 0xFF, SPI_PACKET_MAX_SIZE);
-//			memset(last_img_ptr, 0xFF, SPI_PACKET_MAX_SIZE);
+	//			memset(spi_tx_buff, 0xFF, SPI_PACKET_MAX_SIZE);
+	//			memset(last_img_ptr, 0xFF, SPI_PACKET_MAX_SIZE);
 
-			for(packetId=0; packetId<numPackets; packetId++) {
-				spiSelect(&SPID1);
-				spiExchange(&SPID1, SPI_PACKET_MAX_SIZE, &last_img_ptr[packetId*SPI_PACKET_MAX_SIZE], spi_rx_buff);
-				spiUnselect(&SPID1);
+				for(packetId=0; packetId<numPackets; packetId++) {
+					spiSelect(&SPID1);
+					spiExchange(&SPID1, SPI_PACKET_MAX_SIZE, &last_img_ptr[packetId*SPI_PACKET_MAX_SIZE], spi_rx_buff);
+					spiUnselect(&SPID1);
 
-				// A little pause between transactions is needed for the communication to work, 5000 NOP loops last about 350-500 us.
-				for(delay=0; delay<SPI_DELAY; delay++) {
-					__NOP();
+					// A little pause between transactions is needed for the communication to work, 5000 NOP loops last about 350-500 us.
+					for(delay=0; delay<SPI_DELAY; delay++) {
+						__NOP();
+					}
+
+					if(spi_rx_buff[1] == 0) { // No answer from ESP32...
+						rx_err = 1;
+						break;
+					}
 				}
 
-				if(spi_rx_buff[1] == 0) { // No answer from ESP32...
-					rx_err = 1;
-					break;
+				if(remainingBytes>0 && rx_err==0) {
+					spiSelect(&SPID1);
+					spiExchange(&SPID1, remainingBytes, &last_img_ptr[packetId*SPI_PACKET_MAX_SIZE], spi_rx_buff);
+					spiUnselect(&SPID1);
+
+					// A little pause between transactions is needed for the communication to work, 5000 NOP loops last about 350-500 us.
+					for(delay=0; delay<SPI_DELAY; delay++) {
+						__NOP();
+					}
 				}
+
+				dcmi_release_last_image_ptr();
+
+			} else if(spi_rx_buff[1] == 0xB6) { // -'J' => camera settings
+
 			}
-
-			if(remainingBytes>0 && rx_err==0) {
-				spiSelect(&SPID1);
-				spiExchange(&SPID1, remainingBytes, &last_img_ptr[packetId*SPI_PACKET_MAX_SIZE], spi_rx_buff);
-				spiUnselect(&SPID1);
-
-				// A little pause between transactions is needed for the communication to work, 5000 NOP loops last about 350-500 us.
-				for(delay=0; delay<SPI_DELAY; delay++) {
-					__NOP();
-				}
-			}
-
-			dcmi_release_last_image_ptr();
-
-		} else if(spi_rx_buff[1] == 0xB6) { // -'J' => camera settings
-
 		}
 
 	} // Infinite loop.
@@ -170,6 +182,41 @@ void spi_comm_start(void) {
 		//SPI_CR1_BR_1 | SPI_CR1_BR_0 // 5.25 MHz
 	};
 	spiStart(&SPID1, &hs_spicfg);	// Setup transfer parameters.
-	chThdCreateStatic(spi_thread_wa, sizeof(spi_thread_wa), NORMALPRIO, spi_thread, NULL);
+	spi_thd_ptr = chThdCreateStatic(spi_thread_wa, sizeof(spi_thread_wa), NORMALPRIO, spi_thread, NULL);
 }
+
+void spi_image_transfer_enable() {
+	image_transfer_enabled = 1;
+}
+
+void spi_image_transfer_disable() {
+	image_transfer_enabled = 0;
+}
+
+void spi_rgb_setting_enable(void) {
+	rgb_setting_enabled = 1;
+}
+
+void spi_rgb_setting_disable(void) {
+	rgb_setting_enabled = 0;
+}
+
+void spi_comm_suspend(void) {
+	if(spi_suspend_flag == 0) {
+		spi_suspend_flag = 1;
+		while(spi_thd_ptr->p_state != CH_STATE_SUSPENDED) { // Wait for the thread to be suspended.
+			chThdSleepMilliseconds(2);
+		}
+	}
+}
+
+void spi_comm_resume(void) {
+	if(spi_suspend_flag == 1) {
+		chSysLock();
+		chSchWakeupS(spi_thd_ptr, MSG_OK);
+		spi_suspend_flag = 0;
+		chSysUnlock();
+	}
+}
+
 
--- src/spi_comm.h
+++ src/spi_comm.h
@@ -8,5 +8,11 @@
 #define SPI_PACKET_MAX_SIZE 4092
 
 void spi_comm_start(void);
+void spi_image_transfer_enable(void);
+void spi_image_transfer_disable(void);
+void spi_rgb_setting_enable(void);
+void spi_rgb_setting_disable(void);
+void spi_comm_suspend(void);
+void spi_comm_resume(void);
 
 #endif /* SPI_COMM_H */

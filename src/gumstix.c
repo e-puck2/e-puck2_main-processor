8b8bc175326fbccf778dcaf71f77d3012c16c30d
gctronic <stefano@gctronic.com>
2020-09-02 13:03:35 +0200

    I2C communication changed to be compatible with the e-puck1 protocol implemented by the York lab.
    Bug fix: avoid negative values for proximity reflected

--- src/gumstix.c
+++ src/gumstix.c
@@ -12,6 +12,15 @@
 #include "audio/play_melody.h"
 #include "ir_remote.h"
 #include "button.h"
+#include "epuck1x/motor_led/advance_one_timer/e_led.h"
+
+#define BUFF_SIZE 32
+
+uint8_t leds_outer = 0;
+uint8_t leds_front_body = 0;
+int16_t motor_left = 0;
+int16_t motor_right = 0;
+uint8_t test_err = 0;
 
 /**
  * I2C slave test routine.
@@ -24,17 +33,13 @@
  *          Stretches clock until reply available.
  */
 
-
 #define slaveI2cPort    I2CD1
 #define slaveI2Caddress  0x3E	// 8 bits address (0x1F is the 7-bit address).
-
-#define ACTUATORS_SIZE (19+1) // Data + checksum.
-#define SENSORS_SIZE (46+1) // Data + checksum.
 
 static BSEMAPHORE_DECL(i2c_rx_ready, true);
 binary_semaphore_t *sem = &i2c_rx_ready;
 
-I2CSlaveMsgCB messageProcessor, clearAfterSend, catchError;
+I2CSlaveMsgCB messageProcessor, clearAfterSend, catchError, addrMatchCB;
 
 static const I2CConfig slaveI2Cconfig = {
 		OPMODE_I2C,
@@ -44,22 +49,15 @@ static const I2CConfig slaveI2Cconfig = {
 	STD_DUTY_CYCLE
 };
 
-/* Last message received from the master:
- * | Left speed (2) | Right speed (2) | Speaker (1) | LED1, LED3, LED5, LED7 (1) | LED2 RGB (3) | LED4 RGB (3) | LED6 RGB (3) | LED8 RGB (3) | Settings (1) |
- */
-uint8_t i2c_rx_buff[ACTUATORS_SIZE];
-uint8_t work_buffer[ACTUATORS_SIZE];
-
-/* Message to send to the master:
- * | 8 x Prox (16) | 8 x Amb (16) |	4 x Mic (8) | Selector + button (1) | Left steps (2) | Right steps (2) | TV remote (1) |
- */
-uint8_t i2c_tx_buff[SENSORS_SIZE] = {0};
+uint8_t i2c_rx_buff[BUFF_SIZE];
+uint8_t work_buffer[BUFF_SIZE];
+uint8_t i2c_tx_buff[BUFF_SIZE] = {0};
 
 BaseSequentialStream *chp = NULL;           // Used for serial logging
 
 // Handler when something sent to us
 const I2CSlaveMsg i2c_rx_msg = {
-	ACTUATORS_SIZE,			/* max sizeof received msg body */
+	BUFF_SIZE,						/* max sizeof received msg body */
 	i2c_rx_buff,			/* body of received msg */
 	NULL,					/* do nothing on address match */
 	messageProcessor,		/* Routine to process received messages */
@@ -68,7 +66,7 @@ const I2CSlaveMsg i2c_rx_msg = {
 
 // Response to received messages
 I2CSlaveMsg i2c_tx_msg = { 	/* this is in RAM so size may be updated */
-	SENSORS_SIZE,			/* filled in with the length of the message to send */
+	BUFF_SIZE,						/* filled in with the length of the message to send */
 	i2c_tx_buff,			/* Response message */
 	NULL,					/* do nothing special on address match */
 	NULL, //clearAfterSend,       /* Clear receive buffer once replied */
@@ -100,6 +98,20 @@ void catchError(I2CDriver *i2cp)
   noteI2cError(i2cp->errors);
 }
 
+/**
+ * Callback on address match.
+ */
+void addrMatchCB(I2CDriver *i2cp)
+{
+	if((i2cp->i2c->SR2 & I2C_SR2_TRA) > 0) {
+		set_body_led(0);
+		set_front_led(1);
+	} else {
+		set_body_led(1);
+		set_front_led(0);
+	}
+}
+
 
 /**
  *  Message processor - looks at received message, determines reply as quickly as possible
@@ -109,54 +121,253 @@ void catchError(I2CDriver *i2cp)
  *  Note: Called in interrupt context, so need to be quick!
  */
 void messageProcessor(I2CDriver *i2cp) {
-	(void)i2cp;
-	uint8_t j, i=0;
-	int16_t n;
-	uint8_t checksum = 0;
+	uint8_t len = i2cSlaveBytes(i2cp);
 
-	memcpy(work_buffer, i2c_rx_buff, ACTUATORS_SIZE);
-    chSysLockFromISR();
-    chBSemSignalI(sem);
-    chSysUnlockFromISR();
+	switch(i2c_rx_buff[0]) {
+		case 0: // Outer leds
+			if(len == 1) { // Read request
+				i2c_tx_buff[0] = leds_outer;
+				i2c_tx_msg.size = 1;
+				i2cSlaveReplyI(&I2CD1, &i2c_tx_msg);
+			} else { // Write request
+				leds_outer = i2c_rx_buff[1];
+				memcpy(work_buffer, i2c_rx_buff, BUFF_SIZE);
+			    chSysLockFromISR();
+			    chBSemSignalI(sem);
+			    chSysUnlockFromISR();
+			}
+			break;
+		case 1: // Body, front leds
+			if(len == 1) { // Read request
+				i2c_tx_buff[0] = leds_front_body;
+				i2c_tx_msg.size = 1;
+				i2cSlaveReplyI(&I2CD1, &i2c_tx_msg);
+			} else { // Write request
+				leds_front_body = i2c_rx_buff[1];
+				memcpy(work_buffer, i2c_rx_buff, BUFF_SIZE);
+			    chSysLockFromISR();
+			    chBSemSignalI(sem);
+			    chSysUnlockFromISR();
+			}
+			break;
+		case 2: // Left speed
+			if(len == 1) { // Read request
+				i2c_tx_buff[0] = motor_left&0xFF;
+				i2c_tx_buff[1] = (motor_left>>8)&0xFF;
+				i2c_tx_msg.size = 2;
+				i2cSlaveReplyI(&I2CD1, &i2c_tx_msg);
+			} else { // Write request
+				motor_left = i2c_rx_buff[1]|(i2c_rx_buff[2]<<8);
+				if(motor_left > 1000) {
+					motor_left = 1000;
+				} else if(motor_left < -1000) {
+					motor_left = -1000;
+				}
+				memcpy(work_buffer, i2c_rx_buff, BUFF_SIZE);
+			    chSysLockFromISR();
+			    chBSemSignalI(sem);
+			    chSysUnlockFromISR();
+			}
+			break;
+		case 3: // Right speed
+			if(len == 1) { // Read request
+				i2c_tx_buff[0] = motor_right&0xFF;
+				i2c_tx_buff[1] = (motor_right>>8)&0xFF;
+				i2c_tx_msg.size = 2;
+				i2cSlaveReplyI(&I2CD1, &i2c_tx_msg);
+			} else { // Write request
+				motor_right = i2c_rx_buff[1]|(i2c_rx_buff[2]<<8);
+				if(motor_right > 1000) {
+					motor_right = 1000;
+				} else if(motor_right < -1000) {
+					motor_right = -1000;
+				}
+				memcpy(work_buffer, i2c_rx_buff, BUFF_SIZE);
+			    chSysLockFromISR();
+			    chBSemSignalI(sem);
+			    chSysUnlockFromISR();
+			}
+			break;
+		case 4: // Left steps
+			if(len == 1) { // Read request
+				int16_t steps = left_motor_get_pos();
+				i2c_tx_buff[0] = steps&0xFF;
+				i2c_tx_buff[1] = (steps>>8)&0xFF;
+				i2c_tx_msg.size = 2;
+				i2cSlaveReplyI(&I2CD1, &i2c_tx_msg);
+			}
+			break;
+		case 5: // Right steps
+			if(len == 1) { // Read request
+				int16_t steps = right_motor_get_pos();
+				i2c_tx_buff[0] = steps&0xFF;
+				i2c_tx_buff[1] = (steps>>8)&0xFF;
+				i2c_tx_msg.size = 2;
+				i2cSlaveReplyI(&I2CD1, &i2c_tx_msg);
+			}
+			break;
+		case 6: // IR sensors control => not implemented
+			break;
+		case 7: // Prox0
+			if(len == 1) { // Read request
+				uint16_t prox = get_prox(0);
+				i2c_tx_buff[0] = prox&0xFF;
+				i2c_tx_buff[1] = (prox>>8)&0xFF;
+				i2c_tx_msg.size = 2;
+				i2cSlaveReplyI(&I2CD1, &i2c_tx_msg);
+			}
+			break;
+		case 8: // Prox1
+			if(len == 1) { // Read request
+				uint16_t prox = get_prox(1);
+				i2c_tx_buff[0] = prox&0xFF;
+				i2c_tx_buff[1] = (prox>>8)&0xFF;
+				i2c_tx_msg.size = 2;
+				i2cSlaveReplyI(&I2CD1, &i2c_tx_msg);
+			}
+			break;
+		case 9: // Prox2
+			if(len == 1) { // Read request
+				uint16_t prox = get_prox(2);
+				i2c_tx_buff[0] = prox&0xFF;
+				i2c_tx_buff[1] = (prox>>8)&0xFF;
+				i2c_tx_msg.size = 2;
+				i2cSlaveReplyI(&I2CD1, &i2c_tx_msg);
+			}
+			break;
+		case 10: // Prox3
+			if(len == 1) { // Read request
+				uint16_t prox = get_prox(3);
+				i2c_tx_buff[0] = prox&0xFF;
+				i2c_tx_buff[1] = (prox>>8)&0xFF;
+				i2c_tx_msg.size = 2;
+				i2cSlaveReplyI(&I2CD1, &i2c_tx_msg);
+			}
+			break;
+		case 11: // Prox4
+			if(len == 1) { // Read request
+				uint16_t prox = get_prox(4);
+				i2c_tx_buff[0] = prox&0xFF;
+				i2c_tx_buff[1] = (prox>>8)&0xFF;
+				i2c_tx_msg.size = 2;
+				i2cSlaveReplyI(&I2CD1, &i2c_tx_msg);
+			}
+			break;
+		case 12: // Prox5
+			if(len == 1) { // Read request
+				uint16_t prox = get_prox(5);
+				i2c_tx_buff[0] = prox&0xFF;
+				i2c_tx_buff[1] = (prox>>8)&0xFF;
+				i2c_tx_msg.size = 2;
+				i2cSlaveReplyI(&I2CD1, &i2c_tx_msg);
+			}
+			break;
+		case 13: // Prox6
+			if(len == 1) { // Read request
+				uint16_t prox = get_prox(6);
+				i2c_tx_buff[0] = prox&0xFF;
+				i2c_tx_buff[1] = (prox>>8)&0xFF;
+				i2c_tx_msg.size = 2;
+				i2cSlaveReplyI(&I2CD1, &i2c_tx_msg);
+			}
+			break;
+		case 14: // Prox7
+			if(len == 1) { // Read request
+				uint16_t prox = get_prox(7);
+				i2c_tx_buff[0] = prox&0xFF;
+				i2c_tx_buff[1] = (prox>>8)&0xFF;
+				i2c_tx_msg.size = 2;
+				i2cSlaveReplyI(&I2CD1, &i2c_tx_msg);
+			}
+			break;
+		case 15: // Ambient0
+			if(len == 1) { // Read request
+				uint16_t prox = get_ambient_light(0);
+				i2c_tx_buff[0] = prox&0xFF;
+				i2c_tx_buff[1] = (prox>>8)&0xFF;
+				i2c_tx_msg.size = 2;
+				i2cSlaveReplyI(&I2CD1, &i2c_tx_msg);
+			}
+			break;
+		case 16: // Ambient1
+			if(len == 1) { // Read request
+				uint16_t prox = get_ambient_light(1);
+				i2c_tx_buff[0] = prox&0xFF;
+				i2c_tx_buff[1] = (prox>>8)&0xFF;
+				i2c_tx_msg.size = 2;
+				i2cSlaveReplyI(&I2CD1, &i2c_tx_msg);
+			}
+			break;
+		case 17: // Ambient2
+			if(len == 1) { // Read request
+				uint16_t prox = get_ambient_light(2);
+				i2c_tx_buff[0] = prox&0xFF;
+				i2c_tx_buff[1] = (prox>>8)&0xFF;
+				i2c_tx_msg.size = 2;
+				i2cSlaveReplyI(&I2CD1, &i2c_tx_msg);
+			}
+			break;
+		case 18: // Ambient3
+			if(len == 1) { // Read request
+				uint16_t prox = get_ambient_light(3);
+				i2c_tx_buff[0] = prox&0xFF;
+				i2c_tx_buff[1] = (prox>>8)&0xFF;
+				i2c_tx_msg.size = 2;
+				i2cSlaveReplyI(&I2CD1, &i2c_tx_msg);
+			}
+			break;
+		case 19: // Ambient4
+			if(len == 1) { // Read request
+				uint16_t prox = get_ambient_light(4);
+				i2c_tx_buff[0] = prox&0xFF;
+				i2c_tx_buff[1] = (prox>>8)&0xFF;
+				i2c_tx_msg.size = 2;
+				i2cSlaveReplyI(&I2CD1, &i2c_tx_msg);
+			}
+			break;
+		case 20: // Ambient5
+			if(len == 1) { // Read request
+				uint16_t prox = get_ambient_light(5);
+				i2c_tx_buff[0] = prox&0xFF;
+				i2c_tx_buff[1] = (prox>>8)&0xFF;
+				i2c_tx_msg.size = 2;
+				i2cSlaveReplyI(&I2CD1, &i2c_tx_msg);
+			}
+			break;
+		case 21: // Ambient6
+			if(len == 1) { // Read request
+				uint16_t prox = get_ambient_light(6);
+				i2c_tx_buff[0] = prox&0xFF;
+				i2c_tx_buff[1] = (prox>>8)&0xFF;
+				i2c_tx_msg.size = 2;
+				i2cSlaveReplyI(&I2CD1, &i2c_tx_msg);
+			}
+			break;
+		case 22: // Ambient7
+			if(len == 1) { // Read request
+				uint16_t prox = get_ambient_light(7);
+				i2c_tx_buff[0] = prox&0xFF;
+				i2c_tx_buff[1] = (prox>>8)&0xFF;
+				i2c_tx_msg.size = 2;
+				i2cSlaveReplyI(&I2CD1, &i2c_tx_msg);
+			}
+			break;
+		case 254: // Testing purpose
+			if(len == 1) { // Read request
+				i2c_tx_buff[0] = test_err;
+				i2c_tx_msg.size = 1;
+				i2cSlaveReplyI(&I2CD1, &i2c_tx_msg);
+				if(test_err == 255) {
+					test_err = 0;
+				} else {
+					test_err++;
+				}
+			}
+			break;
+		default:
+			break;
+	}
 
-    for(j=0; j<8; j++) {
-        n = get_calibrated_prox(j);
-        i2c_tx_buff[i++] = n & 0xff;
-        i2c_tx_buff[i++] = n >> 8;
-    }
-    for(j=0; j<8; j++) {
-        n = get_ambient_light(j);
-        i2c_tx_buff[i++] = n & 0xff;
-        i2c_tx_buff[i++] = n >> 8;
-    }
-    for(j=0; j<4; j++) {
-    	n = mic_get_volume(j);
-        i2c_tx_buff[i++] = n & 0xff;
-        i2c_tx_buff[i++] = n >> 8;
-    }
-    i2c_tx_buff[i++] = get_selector(); // First 4 bits used for the selector.
-    if(button_is_pressed()) {	// 5th bit used for the button state.
-    	i2c_tx_buff[i-1] |= 0x10;
-    } else {
-    	i2c_tx_buff[i-1] &= 0xEF;
-    }
-    n = left_motor_get_pos();
-    i2c_tx_buff[i++] = n & 0xff;
-    i2c_tx_buff[i++] = n >> 8;
-    n = right_motor_get_pos();
-    i2c_tx_buff[i++] = n & 0xff;
-    i2c_tx_buff[i++] = n >> 8;
-    i2c_tx_buff[i++] = ir_remote_get_data();
-
-    // Construct a cheksum (Longitudinal Redundancy Check) to put at the end of the message.
-    checksum = 0;
-    for(j=0; j<(SENSORS_SIZE-1); j++) {
-    	checksum ^= i2c_tx_buff[j];
-    }
-    i2c_tx_buff[SENSORS_SIZE-1] = checksum;
-
-    i2c_tx_msg.size = SENSORS_SIZE;
-    i2cSlaveReplyI(&I2CD1, &i2c_tx_msg);
 }
 
 /**
@@ -178,9 +389,6 @@ void start_gumstix_comm(BaseSequentialStream *serport)
 {
   chp = serport;
 
-  int16_t speedl, speedr;
-  uint8_t checksum = 0;
-
   i2cStart(&slaveI2cPort, &slaveI2Cconfig);
 #if HAL_USE_I2C1_MS_SLAVE
   slaveI2cPort.slaveTimeout = MS2ST(100);       // Time for complete message.
@@ -194,7 +402,7 @@ void start_gumstix_comm(BaseSequentialStream *serport)
 
 
   // Prepare the reply for a "write+read" transaction.
-  i2c_tx_msg.size = SENSORS_SIZE;
+  i2c_tx_msg.size = 1;
   i2cSlaveReplyI(&I2CD1, &i2c_tx_msg);
 
   while(true) {
@@ -204,51 +412,28 @@ void start_gumstix_comm(BaseSequentialStream *serport)
     	gotI2cError = 0;
         //chprintf(chp, "I2cError: %04x\r\n", lastI2cErrorFlags);
     } else {
-
-    	// Verify the checksum (Longitudinal Redundancy Check) before applying the received command.
-    	checksum = 0;
-    	for(int k=0; k<(ACTUATORS_SIZE-1); k++) {
-    		checksum ^= work_buffer[k];
+    	switch(work_buffer[0]) {
+    		case 0: // outer leds
+				e_set_led(0, leds_outer&0x01);
+				e_set_led(1, leds_outer&0x02);
+				e_set_led(2, leds_outer&0x04);
+				e_set_led(3, leds_outer&0x08);
+				e_set_led(4, leds_outer&0x10);
+				e_set_led(5, leds_outer&0x20);
+				e_set_led(6, leds_outer&0x40);
+				e_set_led(7, leds_outer&0x80);
+    			break;
+    		case 1: // body, front leds
+				set_front_led(leds_front_body&0x01);
+				set_body_led(leds_front_body&0x02);
+    			break;
+    		case 2: // left speed
+    			left_motor_set_speed(motor_left);
+    			break;
+    		case 3: // right speed
+				right_motor_set_speed(motor_right);
+    			break;
     	}
-    	if(checksum == work_buffer[ACTUATORS_SIZE-1]) {
-    		speedl = (unsigned char)work_buffer[0] + ((unsigned int)work_buffer[1] << 8);
-    		speedr = (unsigned char)work_buffer[2] + ((unsigned int)work_buffer[3] << 8);
-
-    		if(work_buffer[4]>=1 && work_buffer[4]<=3) {
-    			play_melody(work_buffer[4]-1);
-    		}
-
-    		set_led(LED1, (work_buffer[5])&0x01);
-    		set_led(LED3, (work_buffer[5]>>1)&0x01);
-    		set_led(LED5, (work_buffer[5]>>2)&0x01);
-    		set_led(LED7, (work_buffer[5]>>3)&0x01);
-
-    		set_rgb_led(0, work_buffer[6], work_buffer[7], work_buffer[8]);
-    		set_rgb_led(1, work_buffer[9], work_buffer[10], work_buffer[11]);
-    		set_rgb_led(2, work_buffer[12], work_buffer[13], work_buffer[14]);
-    		set_rgb_led(3, work_buffer[15], work_buffer[16], work_buffer[17]);
-
-    		// Handle behaviors and others commands.
-    		if(work_buffer[18] & 0x01) { // Calibrate proximity.
-    			calibrate_ir();
-    		}
-    		if(work_buffer[18] & 0x02) { // Enable obastacle avoidance.
-
-    		} else { // Disable obstacle avoidance
-
-    		}
-    		if(work_buffer[18] & 0x04) { // Set steps.
-    			left_motor_set_pos(speedl);
-    			right_motor_set_pos(speedr);
-    		} else { // Set speed.
-    			left_motor_set_speed(speedl);
-    			right_motor_set_speed(speedr);
-    		}
-
-		} else {
-			//chprintf(chp, "wrong checksum (%02x != %02x)\r\n", work_buffer[ACTUATORS_SIZE-1], checksum);
-    	}
-
     	//chprintf(chp, "SR2: %04x\r\n", (&slaveI2cPort)->i2c->SR2);
     }
 
